36898717f5e0f695e321163429ecfa0a9b51078b
Raluca Bosorogan <ralucabosorogan436@gmail.com>
2026-05-17 23:35:42 +0300

    Add files via upload

---
+++ TodoAPP/Controllers/TodoController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TodoAPP.Models;
+
+namespace TodoAPP.Controllers
+{
+    public class TodoController : Controller
+    {
+        private static readonly List<ToDoItem> _toDoList = new()
+        {
+            new ToDoItem { Id = 1, Nume = "Cumpara produse", EsteFinalizat = false },
+            new ToDoItem { Id = 2, Nume = "Tunde iarba", EsteFinalizat = true }
+        };
+
+        public IActionResult Index() => View(_toDoList);
+
+        [HttpPost]
+        public IActionResult Create(string nouTask)
+        {
+            if (string.IsNullOrWhiteSpace(nouTask))
+                return RedirectToAction("Index");
+
+            _toDoList.Add(new ToDoItem
+            {
+                Id = _toDoList.Count > 0 ? _toDoList.Select(x => x.Id).Max() + 1 : 1,
+                Nume = nouTask,
+                EsteFinalizat = false
+            });
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult ToggleComplete(int id)
+        {
+            var task = _toDoList.Find(x => x.Id == id);
+            if (task != null)
+            {
+                task.EsteFinalizat = !task.EsteFinalizat;
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var task = _toDoList.Find(x => x.Id == id);
+            if (task != null)
+            {
+                _toDoList.Remove(task);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ TodoAPP/Models/ToDoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoAPP.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+        public string Nume { get; set; } = string.Empty;
+        public bool EsteFinalizat { get; set; }
+    }
+}
